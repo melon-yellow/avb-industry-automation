5d21b0bc3bae6301bedbe90c51f10c062a78cde2
anthony <78743473+anthony-freitas@users.noreply.github.com>
2021-11-16 16:55:35 -0300

    asdasfdgdrfbxdzcxzcb gn

---
+++ mill/wincc/preventBillet/lrs_diag/acknowledge/releaseLeft.c
@@ -0,0 +1,13 @@
+#include "apdefap.h"
+void OnLButtonUp(char* lpszPictureName, char* lpszObjectName, char* lpszPropertyName, UINT nFlags, int x, int y)
+{
+
+// Set Variables
+char tagname[71];
+strncpy(tagname, GetText(lpszPictureName, "PBTagPrefix"), 70);
+strcat(tagname, ".ACK_FAULT");
+
+// Set Ack
+SetTagBit(tagname, 0);
+
+}
